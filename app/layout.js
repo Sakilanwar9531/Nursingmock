4df145b9f074c0bf6ec21fbfc1ad7a4cdc17f5e5
Sakilanwar9531 <sakil.net.in@gmail.com>
2026-07-30 01:32:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,1 @@
-export const metadata = {
-  title: "NCBT - National CBT",
-  description: "India's Trusted Platform for Nursing, Pharmacist & Paramedical Government Exam Prep",
-};
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0, backgroundColor: "#f9fafb" }}>
-        {children}
-      </body>
-    </html>
-  );
-}
